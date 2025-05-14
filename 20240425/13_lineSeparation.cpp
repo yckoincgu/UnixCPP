a0bede24f8dbe25672fc8425457532589d24ebf8
柯宇謙 <35418023+yckoincgu@users.noreply.github.com>
2025-05-14 10:48:19 +0800

    20250514_1
    20250514_1

--- 20240425/13_lineSeparation.cpp
+++ 20240425/13_lineSeparation.cpp
@@ -4,19 +4,23 @@
 #include <vector>
 
 int main() {
-    std::string data = "apple,banana,cherry";
-    std::istringstream iss(data);
+    std::string data = "apple,banana,cherry";   // std::string = Container for text (store, modify, access randomly).
+                                                    
+    std::istringstream iss(data);               // Creates a stream from the string
+                                                // Streams = I/O tools for sequential reading/writing (files, parsing).
     std::string fruit;
     std::vector<std::string> fruits;
 
-    while (std::getline(iss, fruit, ',')) {
+    while (std::getline(iss, fruit, ',')) {         // Stops at the first comma
+                                                    // Returns a reference to iss (useful for chaining operations).
+                                                    // after the loop finishes, iss is empty (all data has been extracted).
         fruits.push_back(fruit);
     }
 
     std::cout << "Fruits: ";
     for (const auto& f : fruits) {
         std::cout << f << " ";
-        // // You cannot modify f here, and you are not making a copy by "const auto&"
+        // You cannot modify f here, and you are not making a copy by "const auto&"
     }
     std::cout << std::endl; // Output: Fruits: apple banana cherry
 
